029b9f0d5d685886615b77ecdbffb4ba43724883
Jose Corredor <jos3.corredor@gmail.com>
2023-07-22 13:42:50 -0500

    Se agrega verificaciones y condiciones para validar entrada

--- assets/js/scriptcolors.js
+++ assets/js/scriptcolors.js
@@ -17,52 +17,93 @@ document.addEventListener("keydown",DivChange);
 function DivChange(idevent){
     if (idevent.key === 'a' || idevent.key === 'A') {
         ColorF = "pink";
+        if(!document.querySelector(".NewSpan")){
+            CreateText(idevent.key);
+        }
+        else{
+            ModText(idevent.key);
+        }
         }
     else if (idevent.key === 's' || idevent.key === 'S') {
         ColorF = "orange";
+        if(!document.querySelector(".NewSpan")){
+            CreateText(idevent.key);
+        }
+        else{
+            ModText(idevent.key);
+        }
         }
     else if (idevent.key === 'd' || idevent.key === 'D') {
         ColorF = "skyblue";
+        if(!document.querySelector(".NewSpan")){
+            CreateText(idevent.key);
+        }
+        else{
+            ModText(idevent.key);
+        }
         }
     else if (idevent.key == 'q' || idevent.key =='Q'){
         if (bandera == 0){
                 bandera = 1;
                 CreateDiv("yellow");
+                if(!document.querySelector(".NewSpan")){
+                    CreateText(idevent.key);
+                }
+                else{
+                    ModText(idevent.key);
+                }
             }
         else{
                 let newDiv =document.querySelector(".newDiv");
-                Fondo("yellow",newDiv)
+                Fondo("yellow",newDiv);
+                ModText(idevent.key);
             }
         }
     else if (idevent.key == 'w' || idevent.key =='W'){
         if (bandera == 0){
                 bandera = 1;
                 CreateDiv("blue");
+                if(!document.querySelector(".NewSpan")){
+                    CreateText(idevent.key);
+                }
+                else{
+                    ModText(idevent.key);
+                }
             }
         else{
                 let newDiv =document.querySelector(".newDiv");
-                Fondo("blue",newDiv)
+                Fondo("blue",newDiv);
+                ModText(idevent.key);
             }
         }
     else if (idevent.key == 'e' || idevent.key =='E'){
         if (bandera == 0){
                 bandera = 1;
                 CreateDiv("brown");
+                if(!document.querySelector(".NewSpan")){
+                    CreateText(idevent.key);
+                }
+                else{
+                    ModText(idevent.key);
+                }
             }
         else{
                 let newDiv =document.querySelector(".newDiv");
-                Fondo("brown",newDiv)
+                Fondo("brown",newDiv);
+                ModText(idevent.key);
             }
         }
-    // else{
-    //     alert("tecla no valida");
-    // }
+    else{
+        if(!document.querySelector(".NewSpan")){
+            CreateText(idevent.key, "  no es valida, no reprensenta ninguna acción");
+        }
+        else{
+            ModText(idevent.key, "  no es valida, no reprensenta ninguna acción");
+        }
+        
+    }
     key.style.backgroundColor=ColorF;
 }
-// document.addEventListener("keydown",NewDivElement);
-// function NewDivElement (idevent){
-    
-// }
 
 function CreateDiv(ColorDiv){
     const newElement = document.createElement("div");
@@ -75,11 +116,27 @@ function CreateDiv(ColorDiv){
     newDiv.style.margin ="30px";
     newDiv.style.backgroundColor = ColorDiv;
 }
-
-
+function CreateText(TeclaSelect, Mensaje= " "){
+    let newElement = document.createElement("p");
+    newElement.classList.add("NewP");
+    newElement.textContent= "Seleccionaste la letra ";
+    document.querySelector(".container").appendChild(newElement);
+    let NewP =document.querySelector(".NewP");
+    NewP.style.marginTop ="30px";
+    newElement = document.createElement("span");
+    newElement.classList.add("NewSpan");
+    newElement.textContent= TeclaSelect + Mensaje;
+    document.querySelector(".container").appendChild(newElement);
+    let NewSpan =document.querySelector(".NewSpan");
+    NewSpan.style.marginTop ="30px";
+}
+function ModText(TeclaSelect, Mensaje = " "){
+    let NewSpan =document.querySelector(".NewSpan");
+    NewSpan.innerHTML = TeclaSelect + Mensaje;
+}
 
 function Fondo(colorini= "white",idbox){
-    if (idbox.style.backgroundColor == colorini){
+    if (idbox.style.backgroundColor == colorini && (idbox == box1 || idbox == box2 || idbox == box3 || idbox == box4)){
         idbox.style.backgroundColor = "Black";
     }
     else {
